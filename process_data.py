de7d325387d1837cba19bfa73620ec1f124b51af
Kobe Wauters <kobewauters@gmail.com>
2023-05-19 13:17:16 +0200

    Kobe 2023-05-19

--- process_data.py
+++ process_data.py
@@ -4,71 +4,76 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 
+def phase_unwrap(phase: float) -> float:
+    result = phase
+    if result > np.pi:
+            fit_times = math.floor(result / (2 * np.pi))
+            result -= (fit_times + 1) * 2 * np.pi
+    if result < -np.pi:
+            fit_times = math.floor(-result / (2 * np.pi))
+            result += (fit_times + 1) * 2 * np.pi
+    return result
+
 
 def process_files():
-    for file in os.listdir("C:/dsp/Python Code vital signs/recordings"):
-        with open("C:/dsp/Python Code vital signs/recordings/" + file) as f:
+    dirname_project = os.path.dirname(__file__)
+    dirname_recordings = os.path.join(dirname_project, "recordings")
+    for filename in os.listdir(dirname_recordings):
+        with open(os.path.join(dirname_recordings, filename) ) as f:
             values = np.array([complex(i) for i in f.readlines()])
             breathing_rate, heart_rate = process_rangebin(values)
-            print(f"\nCurrent file: {file}")
+            print(f"\nCurrent file: {filename}")
             print("================================")
             print(f"Breathing rate: {breathing_rate}")
             print(f"Heart rate: {heart_rate}")
 
 
-def process_rangebin(rangebin: np.ndarray):
+def process_rangebin(rangebin: np.ndarray) -> tuple[int,int]:
     """
     Processes the given rangebin and returns the breathing rate and heart rate
     :param rangebin: The numpy array with the complex numbers
     :return: A tuple with the breathing rate and heart rate
     :rtype: tuple
     """
-    phases = np.angle(rangebin)
-
-    for phase in phases:
-        if phase > np.pi:
-            fit_times = math.floor(phase / (2 * np.pi))
-            phase -= (fit_times + 1) * 2 * np.pi
-
-        if phase < -np.pi:
-            fit_times = math.floor(-phase / (2 * np.pi))
-            phase += (fit_times + 1) * 2 * np.pi
+    phases = np.array([phase_unwrap(p) for p in np.angle(rangebin)])
 
     phase_differences = np.subtract(
         phases[0:len(phases) - 1],
         phases[1:]
     )
 
-    breathing_filter = scipy.signal.butter(5, [0.005, 0.03], "bp", fs=20, analog=False, output="sos")
-    heart_filter = scipy.signal.butter(5, [0.04, 5], "bp", fs=20, analog=False, output="sos")
-
-    plt.plot(range(len(phase_differences)), phase_differences)
-    plt.title("The phase differences")
-    plt.show()
+    breathing_filter = scipy.signal.butter(5, [0.05, 0.3], "bp", fs=20, analog=False, output="sos")
+    heart_filter = scipy.signal.butter(5, [0.4, 5], "bp", fs=20, analog=False, output="sos")
 
     filtered_breathing = scipy.signal.sosfilt(breathing_filter, phase_differences)
     filtered_heart = scipy.signal.sosfilt(heart_filter, phase_differences)
 
-    plt.plot(range(len(filtered_breathing)), filtered_breathing)
-    plt.title("The filtered phase differences for the breathing rate")
-    plt.show()
-
-    plt.plot(range(len(filtered_heart)), filtered_heart)
-    plt.title("The filtered phase differences for the heart rate")
-    plt.show()
-
     fft_breathing = np.absolute(scipy.fft.fft(filtered_breathing))
     fft_heart = np.absolute(scipy.fft.fft(filtered_heart))
+    
+    plt.figure(figsize=(16, 8), layout="constrained")
+    plt.subplot(231)
+    plt.plot(range(len(phase_differences)), phase_differences)
+    plt.title("Phase differences")
 
+    plt.subplot(232)
+    plt.plot(range(len(filtered_breathing)), filtered_breathing)
+    plt.title("Filtered phase differences for the breathing rate")
+    
+    plt.subplot(233)
+    plt.plot(range(len(filtered_heart)), filtered_heart)
+    plt.title("Filtered phase differences for the heart rate")
+
+    plt.subplot(235)
     plt.plot(range(len(fft_breathing)), fft_breathing)
-    plt.title("The frequency spectrum for the breathing rate")
-    plt.show()
-
+    plt.title("Frequency spectrum for the breathing rate")
+    
+    plt.subplot(236)
     plt.plot(range(len(fft_heart)), fft_heart)
-    plt.title("The frequency spectrum for the heart rate")
+    plt.title("Frequency spectrum for the heart rate")
     plt.show()
 
-    peaks_breathing, peak_height_breathing = scipy.signal.find_peaks(fft_breathing, 0.5)
+    peaks_breathing, peak_height_breathing = scipy.signal.find_peaks(fft_breathing, height=0.2)
     peaks_heart, peak_height_heart = scipy.signal.find_peaks(fft_heart, 40)
 
     breathing_rate, heart_rate = (0, 0)
